2b409cdaef29c0a873a224ba0bf88b0afcc84509
amogus3000 <bvalentin1415@gmail.com>
2023-12-21 01:31:04 +0200

    added sorting by date

--- app/src/main/java/com/kit/moviestest/data/room/dao/MovieDao.kt
+++ app/src/main/java/com/kit/moviestest/data/room/dao/MovieDao.kt
@@ -1,9 +1,7 @@
 package com.kit.moviestest.data.room.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 import com.kit.moviestest.data.room.entity.Movie
 import kotlinx.coroutines.flow.Flow
 
@@ -15,17 +13,11 @@ interface MovieDao {
     @Query("SELECT * FROM Movies m ORDER BY m.title")
     suspend fun getSortedByTitle(): List<Movie>?
 
-    @Query("SELECT * FROM Movies m ORDER BY m.description") //todo change database and sort by date
+    @Query("SELECT * FROM Movies m ORDER BY m.released_date")
     suspend fun getSortedByDate(): List<Movie>?
 
     @Query("SELECT * FROM Movies m WHERE m.id =:id")
     fun getItemById(id: Int): Flow<Movie?>
-
-    @Insert
-    suspend fun insertMovie(movie: List<Movie>)
-
-    @Update
-    suspend fun updateMovie(movie: Movie)
 
     @Query("UPDATE Movies  SET is_in_watch_list = NOT is_in_watch_list WHERE id=:id")
     fun invertItemBoolean(id: Int)
